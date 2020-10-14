435b4910e8bc9518cf8c884293e76619f1c027ca
俊华 蔡 <1032031912@qq.com>
2020-10-14 11:59:19 +0800

    同步事件/线程池

---
+++ 04_同步事件和等待句柄/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _04_同步事件和等待句柄
+{
+    class Program
+    {
+        //同步事件
+        private static AutoResetEvent waithandle = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.Write("主线程开始:");
+            Thread t = new Thread(DoWork);
+            t.Start();
+            waithandle.WaitOne();//等待waitHandle
+            Console.Write("主线程结束.");Console.ReadKey();
+        }
+        public static void DoWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(".");Thread.Sleep(100);
+            }
+            waithandle.Set();//设置waitHandle
+        }
+
+    }
+}
---
+++ 05_线程池/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _05_线程池
+{
+    class Program
+    {
+        public static void DoWork(object state)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0}-{1}", state.ToString(), i);
+                Thread.Sleep(100);//模拟实际操作
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                ThreadPool.QueueUserWorkItem(DoWork, "线程" + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
